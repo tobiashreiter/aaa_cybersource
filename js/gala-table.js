3ee0b78f8aad88f9f27a7a9a25e72d0359007115
Philip Curley <philip@interactiveknowledge.com>
2023-07-25 11:49:25 -0400

    D8BUGS-358: total amount readonly property

--- js/gala-table.js
+++ js/gala-table.js
@@ -4,8 +4,6 @@
       if (context.querySelector('#edit-gala')) {
         Drupal.behaviors.aaaGalaTable.form = document.querySelector('form.webform-submission-form')
         Drupal.behaviors.aaaGalaTable.amount = Drupal.behaviors.aaaGalaTable.form.querySelector('input[name="amount"]')
-
-        Drupal.behaviors.aaaGalaTable.amount.setAttribute('disabled', true)
         Drupal.behaviors.aaaGalaTable.amount.value = '0'
 
         Drupal.behaviors.aaaGalaTable.form.querySelectorAll('input[type="number"][data-drupal-selector$=quantity]').forEach(function(c) {
